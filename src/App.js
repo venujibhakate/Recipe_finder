c83f257e63e2b83a7f33720cb30b93a4aa89f250
Your Name <you@example.com>
2020-01-14 22:53:56 +0530

    created the searchbar and console the data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect}from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+  const APP_ID = 'f5c945fe';
+  const APP_KEY = '272ae8a6053dc1372ceb9d03337cf652';
+
+
+useEffect(() => {
+  getRecipes()
+});
+
+
+const getRecipes = async () => {
+  const response = await fetch("https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}")
+  const data =  await response.json();
+  console.log(data);
+
+}
+return(
+  <div className="App">
+    <form className='search=form'>
+      <input className='search-bar' type='text'/>
+      <button className='search-button' type='submit'>Search</button>
+    </form>
+
+  </div>
+);
+};
 export default App;
+
+
